e332edd4f3b4fc28fcf144ebf6bf29f105f81c16
Zaher Madi <zahermadi@yahoo.fr>
2026-04-07 11:33:35 +0200

    feat: configure environment variables and Docker setup for development

--- job_board/settings.py
+++ job_board/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v++r+fn6w$j+yr3ye5+_qnqepn*h(n@i$-@hem%b=t3@4za%wp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('APP_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', os.getenv('APP_DOMAIN', 'localhost')]
+
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'https://' + os.getenv('APP_DOMAIN', 'localhost')]
 
 
 # Application definition
